5ba3a3e6fe257340d24904cef33ebd96b94a1b5a
trandinhlinh94 <trandinhlinh94@gmail.com>
2016-06-03 17:01:49 +0300

    create outlet for tableView, action for add button. Add button will show an alert with save and cancel ability

--- HitList/HitList/ViewController.swift
+++ HitList/HitList/ViewController.swift
@@ -8,11 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource  {
+    
+    // MARK: Properties
+    @IBOutlet weak var tableView: UITableView!
+    var names = [String]()
+    
+    
+    // MARK: Actions
+    @IBAction func addName(sender: AnyObject) {
 
+        let alert = UIAlertController(title: "New name", message: "Add a new name", preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .Default, handler: { (action:UIAlertAction) -> Void in
+            let textField = alert.textFields!.first
+            self.names.append(textField!.text!)
+            self.tableView.reloadData()
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Default, handler: {(action: UIAlertAction) -> Void in
+            alert.addTextFieldWithConfigurationHandler { (textField: UITextField) -> Void in
+            }
+        
+        })
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: UITableViewDataSource 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
+        cell!.textLabel?.text = names[indexPath.row]
+        return cell!
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "\"The List\""
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cel")
     }
 
     override func didReceiveMemoryWarning() {
