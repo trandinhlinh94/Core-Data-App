042e9c3c468715e7ad9643a8780d2b35017cbf4e
trandinhlinh94 <trandinhlinh94@gmail.com>
2016-06-13 08:52:18 +0300

    created Person data model and save new records to NSManagedObject. Fetching new records await to be done next

---
+++ HitList/Person+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  Person+CoreDataProperties.swift
+//  HitList
+//
+//  Created by Linh Tran on 13/06/16.
+//  Copyright © 2016 Linh Tran. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Person {
+
+    @NSManaged var name: String?
+
+}
---
+++ HitList/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  HitList
+//
+//  Created by Linh Tran on 13/06/16.
+//  Copyright © 2016 Linh Tran. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Person: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
